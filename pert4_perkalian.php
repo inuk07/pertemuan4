18392cf17eb0f8ae44d4fde002a1552bb2c63800
inuk07 <45460101+inuk07@users.noreply.github.com>
2021-04-22 22:49:53 +0700

    Add files via upload

---
+++ pert4_perkalian.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Pertemuan 4</h1>
+
+<?php
+echo "perbedaan perintah break dan continue adalah jika Perintah continue digunakan untuk men-interupsi perulangan dalam PHP sedangkan perintah break digunakan untuk menghentikan perulangan.";
+?>
+
+</body>
+</html>
